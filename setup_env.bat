5ad258ae5351d5b708e2f607094ec71e895a72d7
Sheamus K. Parkes <shea.parkes@milliman.com>
2017-01-08 23:42:42 -0500

    Utilize pipeline_components_env.bat
    Do this because it removes explicit references to environments and libraries
    from the code.

--- setup_env.bat
+++ setup_env.bat
@@ -6,6 +6,13 @@ rem
 rem ### DEVELOPER NOTES:
 rem   Desired to be as lean as possible since this should be solution agnostic
 
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Setting up environment for usage/testing
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Running from %~f0
 
-call activate prod2016_11
-set PYTHONPATH=%UserProfile%\repos\IndyMacros\Code\python
+rem ### LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
+
+
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Calling current Pipeline_Components_Env
+call S:\PRM\Pipeline_Components_Env\pipeline_components_env.bat
+
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Finished setting up environment for usage/testing
