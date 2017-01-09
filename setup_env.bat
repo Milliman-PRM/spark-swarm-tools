249e3f5f9ad51d4072a4968d532f3aee35a61c7d
Sheamus K. Parkes <shea.parkes@milliman.com>
2017-01-08 23:56:34 -0500

    Go back to prod2016_11.
    Do this because it runs as a user with severly limited network access.

--- setup_env.bat
+++ setup_env.bat
@@ -6,6 +6,7 @@ rem   Do minimal environment setup
 rem
 rem ### DEVELOPER NOTES:
 rem   Desired to be as lean as possible since this should be solution agnostic
+rem   Also runs with very limited network access in production
 
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Setting up environment for usage/testing
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Running from %~f0
@@ -13,7 +14,8 @@ echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Running from %~f0
 rem ### LIBRARIES, LOCATIONS, LITERALS, ETC. GO ABOVE HERE
 
 
-echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Calling current Pipeline_Components_Env
-call S:\PRM\Pipeline_Components_Env\pipeline_components_env.bat
+set CONDA_ENVIRONMENT=prod2016_11
+echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Activating %CONDA_ENVIRONMENT% conda environment
+call activate prod2016_11
 
 echo %~nx0 %DATE:~-4%-%DATE:~4,2%-%DATE:~7,2% %TIME%: Finished setting up environment for usage/testing
