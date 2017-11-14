0b68bb7847955dacb68775e8ce44f88d91b5c742
Steve Gredell <steve.gredell@milliman.com>
2017-11-14 16:26:55 -0500

    Create python package for swarm tools
    Do this to share code between swarm scanners

---
+++ python/__init__.py
@@ -0,0 +1,9 @@
+"""
+## CODE OWNERS: Steve Gredell
+
+### OBJECTIVE:
+  Mark the root of the python library.
+
+  ### DEVELOPER NOTES:
+  Nothing in this library should have side effects when imported.
+"""
---
+++ python/swarm/__init__.py
@@ -0,0 +1,9 @@
+"""
+## CODE OWNERS: Steve Gredell
+
+### OBJECTIVE:
+  Mark the swarm package so it is importable
+
+  ### DEVELOPER NOTES:
+  Nothing in this library should have side effects when imported.
+"""
